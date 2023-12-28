421c66c5574de1676dcb12378172b9b470cff5ee
Danil Klimenko <sos.danil@yandex.ru>
2023-12-29 01:00:28 +0500

    добавил тест к get_five_last_operations

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 from config import TEST_DICTIONARY_PATH
-from src.utils import get_all_operations, get_only_executed_operations, get_sorted_operations
+from src.utils import get_all_operations, get_only_executed_operations, get_sorted_operations, get_five_last_operations
 
 test_list_for_executed = [{"state": "EXECUTED"}, {"state": "CANCELED"}, {}]
 test_list_for_sorted = [{"date": "2015-06-12"}, {"date": "2020-05-07"}, {"date": "2017-02-10"}]
@@ -17,3 +17,7 @@ def test_get_only_executed_operations():
 def test_get_sorted_operations():
     assert get_sorted_operations(test_list_for_sorted) == [{"date": "2020-05-07"}, {"date": "2017-02-10"},
                                                            {"date": "2015-06-12"}]
+
+
+def test_get_five_last_operations():
+    assert get_five_last_operations([1, 2, 3, 4, 5, 6, 7, 8]) == [1, 2, 3, 4, 5]
