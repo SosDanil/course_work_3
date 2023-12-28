a7aa837b744e90d45967e8981cb96bfca9eb7c3f
Danil Klimenko <sos.danil@yandex.ru>
2023-12-28 17:05:15 +0500

    добавил encoding

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import json
 
 
 def get_all_operations(path):
-    with open(path) as file:
+    with open(path, encoding="UTF-8") as file:
         content = file
         all_operations = json.load(content)
         return all_operations
