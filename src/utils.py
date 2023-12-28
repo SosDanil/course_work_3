f164cb6b757c1de974d1f6bace8e75fb048b1161
Danil Klimenko <sos.danil@yandex.ru>
2023-12-28 22:33:38 +0500

    создал вторую функцию и тест к ней

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+from config import OPERATIONS_PATH
 
 
 def get_all_operations(path):
@@ -7,3 +8,16 @@ def get_all_operations(path):
         content = file
         all_operations = json.load(content)
         return all_operations
+
+
+def get_only_executed_operations(all_operations):
+    """получаем список только исполненных операций, без отмен"""
+    executed_operations = []
+    for dictionary in all_operations:
+        if dictionary.get("state") == "EXECUTED":
+            executed_operations.append(dictionary)
+    return executed_operations
+
+
+all_operations = get_all_operations(OPERATIONS_PATH)
+print(get_only_executed_operations(all_operations))
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,12 @@
 from config import TEST_DICTIONARY_PATH
-from src.utils import get_all_operations
+from src.utils import get_all_operations, get_only_executed_operations
 
 
 def test_get_all_operations():
     assert get_all_operations(TEST_DICTIONARY_PATH) == [{"name": "Danil", "surname": "Klimenko"},
                                                         {"fruit": "apple", "animal": "dog"}]
+
+
+test_list_for_executed = [{"state": "EXECUTED"}, {"state": "CANCELED"}, {}]
+def test_get_only_executed_operations():
+    assert get_only_executed_operations(test_list_for_executed) == [{"state": "EXECUTED"}]
